9a2091c533116190b80d409d913c512714652ede
Parikshit Juluri <pari685@gmail.com>
2015-06-02 14:13:59 -0500

    minor

--- client/config_client.py
+++ client/config_client.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 #
-#   CASH n DASH
-#   Parikshit-Sheyda-Rohit
+#   Cache n DASH
+#   Parikshit
+#	Sheyda
+#   Rohit
 #    This program is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation; either version 2 of the License, or
