33c0bff8254065adf1e8992251f59beeabf540ef
Parikshit <pari685@gmail.com>
2016-08-09 18:47:55 -0400

    Update config_client.py
    adding the RE_BUFFERING_COUNT

--- dist/client/config_client.py
+++ dist/client/config_client.py
@@ -84,6 +84,7 @@ JSON_HANDLE['playback_info'] = {'start_time': None,
 BASIC_THRESHOLD = 10
 BASIC_LOWER_THRESHOLD = 0.8
 BASIC_UPPER_THRESHOLD = 1.1
+RE_BUFFERING_COUNT = 0.8
 
 # Number of segments for moving average
 BASIC_DELTA_COUNT = 5
